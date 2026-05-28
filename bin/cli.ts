70bbe17b5e2a9e8d025645aa7ebc03efe3b29da4
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2026-05-28 16:10:38 -0500

    chore: configure commander

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,2 +1,15 @@
 #!/usr/bin/env node
 // Run with: pnpm dev:accessors -- User.ts
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('Dubex')
+  .description(
+    'A JavaScript and TypeScript CLI toolkit for generating boilerplate, accessors, and backend resources.'
+  )
+  .version('0.1.0');
+
+program.parse();
