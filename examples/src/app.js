48c844cc6f5e05718bb94b8432c8db69ba4254fa
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2026-06-06 09:05:34 -0500

    refactor: remove output spacing

--- examples/src/app.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import userRouter from "./routes/userRoutes.js";
-
-const app = express();
-app.use(express.json());
-
-app.use("/api/v1/users", userRouter);
-
-export default app;
--- src/utils/output.ts
+++ src/utils/output.ts
@@ -2,7 +2,6 @@ import chalk from 'chalk';
 import type { GenerationResult } from '../generators/resource/resource.types.js';
 
 export const outputGenerationSummary = (entity: string, results: GenerationResult) => {
-  console.log();
   console.log(`Generated resource: ${chalk.blue(entity)}\n`);
 
   if (results.createdDirs.length > 0) {
@@ -11,7 +10,6 @@ export const outputGenerationSummary = (entity: string, results: GenerationResul
       console.log(chalk.green(`  - ${dir}`));
     }
   }
-  console.log();
 
   if (results.createdFiles.length > 0) {
     console.log('Created files: ');
@@ -19,7 +17,6 @@ export const outputGenerationSummary = (entity: string, results: GenerationResul
       console.log(chalk.green(`  - ${file}`));
     }
   }
-  console.log();
 
   if (results.skippedDirs.length > 0) {
     console.log('Skipped directories: ');
@@ -27,7 +24,6 @@ export const outputGenerationSummary = (entity: string, results: GenerationResul
       console.log(chalk.gray(`  - ${dir}`));
     }
   }
-  console.log();
 
   if (results.skippedFiles.length > 0) {
     console.log('Skipped files: ');
